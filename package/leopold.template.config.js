5d236d86c6133a5a41d5c46ee8e18bfa866dcd0b
leobod <leobod@eside.cn>
2024-01-01 23:35:22 +0800

    chore: 尝试使用新思路处理静态路径与动态路径

--- package/leopold.template.config.js
+++ package/leopold.template.config.js
@@ -73,7 +73,7 @@ module.exports = {
     DynamicRoutes: {
       enabled: true,
       config: {
-        routes: [{ expr: '/api/*', mapping: './api/*' }]
+        routes: [{ dir: './api' }]
       }
     },
     ErrorHandler: {
--- package/middleware/DynamicRoutes.ts
+++ package/middleware/DynamicRoutes.ts
@@ -1,88 +1,57 @@
-const path = require('path');
+const p = require('path');
 const fs = require('fs');
+const pathMatch = require('path-match');
+const { globSync } = require('glob');
 
-function matchRoute(path, routes) {
-  for (let i = 0; i < routes.length; i++) {
-    const route = routes[i];
-    if (route && route.expr) {
-      const pattern = new RegExp('^' + route.expr.replace(/\*/g, '.*') + '$');
-      if (pattern.test(path)) {
-        return route;
-      }
-    }
-  }
-  return null;
+interface DynamicRoutesConfigDef {
+  routes: Array<{ dir: string }>;
 }
-// 示例用法
-// const routePath = '/users/123';
-// const routeExpressions = ['/users/*', '/posts/*', '/settings'];
 
-const fileExtension = (val) => {
-  const dotLastIndex = val.lastIndexOf('.');
-  return dotLastIndex !== -1 ? val.substring(val.lastIndexOf('.') + 1) : '';
+const route = pathMatch()
+
+const _getRenderFilePath = async (app, server, config: DynamicRoutesConfigDef = { routes: [] }) => {
+  const { routes } = config;
+  const routeList = routes ? routes : [];
+  let routeFileList: Array<any> = [];
+  for (const item of routeList) {
+    const currentDir = p.join(process.cwd(), item.dir);
+    const fileList = globSync(['**/*.js'], {
+      ignore: 'node_modules/**',
+      cwd: currentDir,
+      absolute: false
+    });
+    const fileListPost = fileList.map((fileItem: string) => {
+      const fileItemPath = fileItem.replace(/]/g, '').replace(/\[/g, ':').replace(/\\/g, '/').replace(/.js/g, '');
+      return {
+        regPath: `${item.dir}/${fileItemPath}`,
+        originPath: p.join(process.cwd(), item.dir, fileItem)
+      };
+    });
+    routeFileList = [...routeFileList, ...fileListPost];
+  }
+  app.routes = routeFileList.map((item) => {
+    return {
+      match: route(item.regPath),
+      regPath: item.regPath,
+      originPath: item.originPath,
+      fn: require(decodeURIComponent(item.originPath))
+    };
+  });
 };
 
-const _parser = async (ctx, next) => {
-  const { routes = [], config } = ctx.$root;
-  const ext = fileExtension(ctx.request.url).toLowerCase();
-  const url = new URL('http://0.0.0.0' + ctx.request.url);
-  const urlPieces = url.pathname.split('/');
-  const lastPiece = urlPieces[urlPieces.length - 1];
-  const matched = matchRoute(url.pathname, routes);
-  if (matched && routes && routes.length > 0) {
-    const exprDir = matched.expr.replaceAll('*', '');
-    const mappingDir = matched.mapping ? matched.mapping.replaceAll('*', '') : exprDir;
-    const filePath = url.pathname.replace(exprDir, mappingDir);
-    const fileFullPath = decodeURIComponent(path.join(config.path, filePath));
-    if (['html', 'htm', 'txt', 'md'].indexOf(ext) !== -1) {
-      if (fs.existsSync(fileFullPath)) {
-        const content = fs.readFileSync(fileFullPath, 'utf8');
-        ctx.body = content;
-      } else {
-        // ctx.status = 400;
-        await next();
-      }
-    } else if (ext === 'js') {
-      if (fs.existsSync(fileFullPath)) {
-        const fn = require(fileFullPath);
-        await fn(ctx);
-        await next();
-      } else {
-        // ctx.status = 400;
-        await next();
-      }
-    } else if (ext === '') {
-      let fn: Function | null = null;
-      /* 默认路径1获取 */
-      const urlPiecesWithoutLast = [...urlPieces];
-      urlPiecesWithoutLast[urlPieces.length - 1] = '';
-      const urlWithoutLast = urlPiecesWithoutLast.join('/');
-      const defaultEntryUrl = new URL('http://0.0.0.0' + urlWithoutLast + 'index.js');
-      const defaultEntryPath = defaultEntryUrl.pathname.replace(exprDir, mappingDir);
-      const defaultEntry = decodeURIComponent(path.join(config.path, defaultEntryPath));
-      const controllerKey = lastPiece === '' || lastPiece === 'index' ? 'index' : lastPiece;
-      const fileFullPathWithExt = lastPiece ? fileFullPath + '.js' : fileFullPath + 'index.js';
-      if (fs.existsSync(defaultEntry)) {
-        const controller = require(defaultEntry);
-        if (!(controller instanceof Function)) {
-          fn = controller[controllerKey];
-        }
-      }
-      if (!fn && fs.existsSync(fileFullPathWithExt)) {
-        fn = require(fileFullPathWithExt);
-      }
-      if (fn) {
-        await fn(ctx);
-        await next();
-      } else {
-        // ctx.status = 400;
-        await next();
-      }
-    } else {
-      ctx.body = 'unsupported request';
+const _render = async (ctx, next) => {
+  const app = ctx.$root;
+  let matched = null;
+  for (const r of app.routes) {
+    matched = r.match(ctx.url);
+    if (matched) {
+      ctx.matched = matched;
+      await r.fn(ctx);
       await next();
+      break
     }
-  } else {
+  }
+  if (!matched) {
     await next();
   }
 };
@@ -97,10 +66,8 @@ export const DynamicRoutes = {
    */
   init: function (app, server, config = { routes: [] }, enabled = true) {
     if (enabled) {
-      const { routes } = config;
-      const root = server.$root;
-      root.routes = routes ? routes : [];
-      server.use(_parser);
+      _getRenderFilePath(app, server, config);
+      server.use(_render);
     }
   }
 };
