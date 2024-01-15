90c81ec469a901721c8c15520af72727ec0a54d8
leobod <leobod@eside.cn>
2024-01-16 00:05:42 +0800

    feat: 跨域配置需要进一步思考

--- package/middleware/Cors.ts
+++ package/middleware/Cors.ts
@@ -2,8 +2,8 @@ const cors = async (ctx, next) => {
   ctx.set('Server', 'v1.0.0');
   ctx.set('PoweredBy', 'base');
   ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
-  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  // ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With, LZ-KEY, LZ-VAL');
+  // ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   if (ctx.method == 'OPTIONS') {
     ctx.body = 200;
   } else {
