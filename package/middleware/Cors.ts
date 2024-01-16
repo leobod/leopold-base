49cd0dbd7b99536837acd7af23617789f0ba7fa0
LEOBOD <leobod@eside.cn>
2024-01-16 22:06:04 +0800

    feat: 允许跨域

--- package/middleware/Cors.ts
+++ package/middleware/Cors.ts
@@ -2,8 +2,11 @@ const cors = async (ctx, next) => {
   ctx.set('Server', 'v1.0.0');
   ctx.set('PoweredBy', 'base');
   ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', '*')
+  ctx.set('Access-Control-Expose-Headers', '*')
+  ctx.set('Access-Control-Max-Age', 60)
   // ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With, LZ-KEY, LZ-VAL');
-  // ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   if (ctx.method == 'OPTIONS') {
     ctx.body = 200;
   } else {
