9ffe8492fd56a3d108483ca3a091d50a1622d9bd
LEOBOD <leobod@eside.cn>
2024-07-08 23:56:05 +0800

    feat: 补充SQL事务执行过程的错误回收处理,拆分Service来更好的实现对SQL语句的测试和后期的灵活调用

--- package/model/Service.ts
+++ package/model/Service.ts
@@ -21,6 +21,7 @@ export class Service extends ServiceDef {
   error_message: string
   page_num_key: string
   page_size_key: string
+  order_by_key: string
   update_time_modify: boolean
   update_time_key: string
   soft_remove_key: string
@@ -35,9 +36,7 @@ export class Service extends ServiceDef {
       error_message: 'Unknown Error',
       page_num_key: 'page_num',
       page_size_key: 'page_size',
-      order_by_has: true,
-      order_by_key: 'create_at',
-      order_by_val: 'ASC',
+      order_by_key: '',
       update_time_modify: true,
       update_time_key: 'update_at',
       soft_remove_key: 'state',
@@ -70,19 +69,32 @@ export class Service extends ServiceDef {
     this.soft_remove_val = finalOpts.soft_remove_val
     this.page_num_key = formatKeyCase(finalOpts.page_num_key, this.format)
     this.page_size_key = formatKeyCase(finalOpts.page_size_key, this.format)
+    this.order_by_key = finalOpts.order_by_key || ''
     this.update_time_key = formatKeyCase(finalOpts.update_time_key, this.format)
     this.soft_remove_key = formatKeyCase(finalOpts.soft_remove_key, this.format)
     this.pk = formatKeyCase(finalOpts.pk, this.format)
   }
+
+  /**
+   * 创建表格SQL
+   * @param params
+   * @param opts
+   * @returns {SQLModel}
+   */
+  getCreateSQLModel(params = {}, opts = {}): SQLModel {
+    const { model } = this
+    return model.create()
+  }
+
   /**
    * 创建表格
    * @param ctx {Context}
+   * @param params
    * @param opts
    * @returns {Promise<*>}
    */
   async create(ctx: Context, params = {}, opts = {}): Promise<any> {
-    const { model } = this
-    const sql = model.create().toSql()
+    const sql = this.getCreateSQLModel().toSql()
     try {
       return await ctx.db.mysql.query(sql.sql, sql.bindings)
     } catch (e) {
@@ -95,6 +107,20 @@ export class Service extends ServiceDef {
   }
 
   /**
+   * 记录总数SQL
+   * @param params
+   * @param opts
+   * @returns {SQLModel}
+   */
+  getCountSQLModel(params = {}, opts = {}): SQLModel {
+    const { model, page_num_key, page_size_key } = this
+    let pageCond = Object.assign({}, params)
+    delete pageCond[page_num_key]
+    delete pageCond[page_size_key]
+    return model.count().where(reverseFormatObjCase(params, this.format))
+  }
+
+  /**
    * 记录总数
    * @param ctx {Context}
    * @param params
@@ -102,13 +128,47 @@ export class Service extends ServiceDef {
    * @returns {Promise<*>}
    */
   async count(ctx: Context, params = {}, opts = {}): Promise<any> {
-    const { model } = this
-    const sql = model.count().where(reverseFormatObjCase(params, this.format)).toSql()
+    const sql = this.getCountSQLModel().toSql()
     const res = await ctx.db.mysql.query(sql.sql, sql.bindings)
     if (res && res.length > 0) {
       return res[0].total
     } else {
       return null
+    }
+  }
+
+  /**
+   * 记录列表SQL
+   * @param params
+   * @param opts
+   * @returns {SQLModel}
+   */
+  getListSQLModel(
+    params = {},
+    opts = {
+      order_by_key: '',
+      table_column: ['*']
+    }
+  ): SQLModel {
+    const { model, page_num_key, page_size_key } = this
+    const page_num = params[page_num_key]
+    const page_size = params[page_size_key]
+    let listParams = Object.assign({}, params)
+    delete listParams[page_num_key]
+    delete listParams[page_size_key]
+    const order_by_key = opts.order_by_key ? opts.order_by_key : this.order_by_key
+    const table_column = opts.table_column || ['*']
+    if (order_by_key) {
+      return model
+        .select(table_column)
+        .where(reverseFormatObjCase(listParams, this.format))
+        .page(page_num, page_size)
+        .orderBy(order_by_key)
+    } else {
+      return model
+        .select(table_column)
+        .where(reverseFormatObjCase(listParams, this.format))
+        .page(page_num, page_size)
     }
   }
 
@@ -123,52 +183,29 @@ export class Service extends ServiceDef {
     ctx: Context,
     params = {},
     opts = {
-      order_by_has: true,
-      order_by_key: 'create_at',
-      order_by_val: 'ASC',
+      order_by_key: '',
       table_column: ['*']
     }
   ): Promise<any> {
-    const { page_num_key, page_size_key } = this
-    const order_by_has = opts.order_by_has
-    const order_by_key = formatKeyCase(opts.order_by_key || 'create_at', this.format)
-    const order_by_val = formatKeyCase(opts.order_by_val || 'ASC', this.format)
-    const table_column = opts.table_column || ['*']
-    const { model } = this
+    const { model, page_size_key } = this
+    const page_size = params[page_size_key]
+    const countSql = this.getCountSQLModel(params, opts).toSql()
+    const listSql = this.getListSQLModel(params, opts).toSql()
     try {
-      const page_num = params[page_num_key]
-      const page_size = params[page_size_key]
-      let pageCond = Object.assign({}, params)
-      delete pageCond[page_num_key]
-      delete pageCond[page_size_key]
-      pageCond = reverseFormatObjCase(pageCond, this.format)
-      const countResult = await this.count(ctx, pageCond)
+      let countResult = null
+      const countRes = await ctx.db.mysql.query(countSql.sql, countSql.bindings)
+      if (countRes && countRes.length > 0) {
+        countResult = countRes[0].total
+      } else {
+        countResult = null
+      }
       let total_page = 1
       let total_size = countResult
-      if (page_size && page_size > 0) {
+      if (total_size && page_size && page_size > 0) {
         total_page = Math.ceil(total_size / page_size)
       }
-      const res = {
-        list: null,
-        total_page,
-        total_size
-      }
-      let sql: any = { sql: '', bindings: [] }
-      if (order_by_has) {
-        sql = model
-          .select(table_column)
-          .where(reverseFormatObjCase(params, this.format))
-          .page(page_num, page_size)
-          .orderBy(reverseFormatKeyCase(order_by_key, this.format), order_by_val)
-          .toSql()
-      } else {
-        sql = model
-          .select(table_column)
-          .where(reverseFormatObjCase(params, this.format))
-          .page(page_num, page_size)
-          .toSql()
-      }
-      const dbResult = await ctx.db.mysql.query(sql.sql, sql.bindings)
+      const res = { list: null, total_page, total_size }
+      const dbResult = await ctx.db.mysql.query(listSql.sql, listSql.bindings)
       const userFilter = model._filter || {}
       res.list = filterDbResult(dbResult, userFilter)
       return formatObjCase(res, this.format)
@@ -182,7 +219,33 @@ export class Service extends ServiceDef {
   }
 
   /**
-   * 查询单个model
+   * 查询单个记录SQL
+   * @param params
+   * @param opts
+   * @returns {SQLModel}
+   */
+  getListOneSQLModel(
+    params = {},
+    opts = {
+      order_by_key: '',
+      table_column: ['*']
+    }
+  ): SQLModel {
+    const { model } = this
+    const order_by_key = opts.order_by_key ? opts.order_by_key : this.order_by_key
+    const table_column = opts.table_column || ['*']
+    if (order_by_key) {
+      return model
+        .select(table_column)
+        .where(reverseFormatObjCase(params, this.format))
+        .orderBy(order_by_key)
+    } else {
+      return model.select(table_column).where(params)
+    }
+  }
+
+  /**
+   * 查询单个记录
    * @param ctx {Context}
    * @param params
    * @param opts
@@ -192,28 +255,13 @@ export class Service extends ServiceDef {
     ctx: Context,
     params = {},
     opts = {
-      order_by_has: true,
-      order_by_key: 'create_at',
-      order_by_val: 'ASC',
+      order_by_key: '',
       table_column: ['*']
     }
   ): Promise<any> {
-    const order_by_has = opts.order_by_has
-    const order_by_key = formatKeyCase(opts.order_by_key || 'create_at', this.format)
-    const order_by_val = formatKeyCase(opts.order_by_val || 'ASC', this.format)
-    const table_column = opts.table_column || ['*']
     const { model } = this
+    let sql = this.getListOneSQLModel(params, opts).toSql()
     try {
-      let sql: any = { sql: '', bindings: [] }
-      if (order_by_has) {
-        sql = model
-          .select(table_column)
-          .where(reverseFormatObjCase(params, this.format))
-          .orderBy(reverseFormatKeyCase(order_by_key, this.format), order_by_val)
-          .toSql()
-      } else {
-        sql = model.select(table_column).where(params).toSql()
-      }
       const dbResult = await ctx.db.mysql.query(sql.sql, sql.bindings)
       const userFilter = model._filter || {}
       const filterResult = filterDbResult(dbResult, userFilter)
@@ -232,13 +280,12 @@ export class Service extends ServiceDef {
   }
 
   /**
-   * 新建
-   * @param ctx {Context}
+   * 新建SQL
    * @param params
    * @param opts
-   * @returns {Promise<*>}
+   * @returns {SQLModel}
    */
-  async add(ctx: Context, params = {}, opts = {}): Promise<any> {
+  getAddSQLModel(params = {}, opts = {}): SQLModel {
     const { model } = this
     const saveParams = reverseFormatObjCase(params, this.format)
     const rules = getRequiredRules(model)
@@ -258,38 +305,47 @@ export class Service extends ServiceDef {
           modelObj[key] = saveParams[key]
         }
       }
-      const sql = model.insert(modelObj).toSql()
-      try {
-        const dbResult = await ctx.db.mysql.query(sql.sql, sql.bindings)
-        /* OkPacket { fieldCount: 0, affectedRows: 1, insertId: 0, serverStatus: 2,
-                    warningCount: 0, message: '', protocol41: true, changedRows: 0 } */
-        if (dbResult && dbResult.affectedRows > 0) {
-          return 'success'
-        } else {
-          return null
-        }
-      } catch (e) {
-        if (this.error_custom) {
-          throw new Error(this.error_message)
-        } else {
-          throw e
-        }
-      }
+      return model.insert(modelObj)
     } else {
       throw new Error(errMsg as string)
     }
   }
 
   /**
-   * 更新
+   * 新建
    * @param ctx {Context}
    * @param params
    * @param opts
    * @returns {Promise<*>}
    */
-  async edit(ctx: Context, params = {}, opts = {}): Promise<any> {
-    const { pk, update_time_modify, update_time_key } = this
-    const { model } = this
+  async add(ctx: Context, params = {}, opts = {}): Promise<any> {
+    const sql = this.getAddSQLModel(params, opts).toSql()
+    try {
+      const dbResult = await ctx.db.mysql.query(sql.sql, sql.bindings)
+      /* OkPacket { fieldCount: 0, affectedRows: 1, insertId: 0, serverStatus: 2,
+                    warningCount: 0, message: '', protocol41: true, changedRows: 0 } */
+      if (dbResult && dbResult.affectedRows > 0) {
+        return 'success'
+      } else {
+        return null
+      }
+    } catch (e) {
+      if (this.error_custom) {
+        throw new Error(this.error_message)
+      } else {
+        throw e
+      }
+    }
+  }
+
+  /**
+   * 编辑SQLModel
+   * @param params
+   * @param opts
+   * @returns {SQLModel}
+   */
+  getEditSQLModel(params = {}, opts = {}): SQLModel {
+    const { model, pk, update_time_modify, update_time_key } = this
     let updateParams = params
     const condParams = { [pk]: updateParams[pk] }
     if (update_time_modify && update_time_key && !updateParams[update_time_key]) {
@@ -311,10 +367,18 @@ export class Service extends ServiceDef {
       }
     }
     delete modelObj[pk]
-    const sql = model
-      .update(modelObj)
-      .where(reverseFormatObjCase(condParams, this.format))
-      .toSql()
+    return model.update(modelObj).where(reverseFormatObjCase(condParams, this.format))
+  }
+
+  /**
+   * 更新
+   * @param ctx {Context}
+   * @param params
+   * @param opts
+   * @returns {Promise<*>}
+   */
+  async edit(ctx: Context, params = {}, opts = {}): Promise<any> {
+    const sql = this.getEditSQLModel(params, opts).toSql()
     try {
       const dbResult = await ctx.db.mysql.query(sql.sql, sql.bindings)
       /* OkPacket { fieldCount: 0, affectedRows: 1, insertId: 0, serverStatus: 2,
@@ -334,13 +398,12 @@ export class Service extends ServiceDef {
   }
 
   /**
-   * 软移除
-   * @param ctx {Context}
+   * 软移除SQLModel
    * @param params
    * @param opts
-   * @returns {Promise<*>}
+   * @returns {SQLModel}
    */
-  async softRemove(ctx: Context, params = {}, opts = {}): Promise<any> {
+  getSoftRemoveSQLModel(params = {}, opts = {}): SQLModel {
     const {
       pk,
       update_time_modify,
@@ -355,10 +418,20 @@ export class Service extends ServiceDef {
       updateParams[update_time_key] = formatDate(new Date(), 'YYYY/MM/DD HH:mm:ss')
     }
     const modelObj = { [soft_remove_key]: soft_remove_val }
-    const sql = model
+    return model
       .update(reverseFormatObjCase(modelObj, this.format))
       .where(reverseFormatObjCase(condParams, this.format))
-      .toSql()
+  }
+
+  /**
+   * 软移除
+   * @param ctx {Context}
+   * @param params
+   * @param opts
+   * @returns {Promise<*>}
+   */
+  async softRemove(ctx: Context, params = {}, opts = {}): Promise<any> {
+    const sql = this.getSoftRemoveSQLModel(params, opts).toSql()
     try {
       const dbResult = await ctx.db.mysql.query(sql.sql, sql.bindings)
       /* OkPacket { fieldCount: 0, affectedRows: 1, insertId: 0, serverStatus: 2,
@@ -378,6 +451,18 @@ export class Service extends ServiceDef {
   }
 
   /**
+   * 移除SQLModel
+   * @param params
+   * @param opts
+   * @returns {SQLModel}
+   */
+  getRemoveSQLModel(params = {}, opts = {}): SQLModel {
+    const { model, pk } = this
+    const condParams = { [pk]: params[pk] }
+    return model.remove().where(reverseFormatObjCase(condParams, this.format))
+  }
+
+  /**
    * 移除
    * @param ctx {Context}
    * @param params
@@ -385,13 +470,7 @@ export class Service extends ServiceDef {
    * @returns {Promise<*>}
    */
   async remove(ctx: Context, params = {}, opts = {}): Promise<any> {
-    const { pk } = this
-    const { model } = this
-    const condParams = { [pk]: params[pk] }
-    const sql = model
-      .remove()
-      .where(reverseFormatObjCase(condParams, this.format))
-      .toSql()
+    const sql = this.getRemoveSQLModel(params, opts).toSql()
     try {
       const dbResult = await ctx.db.mysql.query(sql.sql, sql.bindings)
       if (dbResult) {
